2645206654a338c73a47e86fc6cd77812670a1c5
Irene Brignoli <ire.brignoli@gmail.com>
2023-02-21 16:11:33 +0100

    aggiunto un pò di stile - es concluso

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@
 Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
-Infine scrivi sulla pagina nomecognomecolorepreferito23*/
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
 
-//name
+
+
+
+//name e surname
 
 const firstName = prompt('Inserisci nome');
 
 console.log( 'La variabile firstName vale: ' + firstName);
 
-//surname
-
 const surName = prompt('Inserisci cognome');
 
 console.log( 'La variabile surName vale: ' + surName);
+
+document.getElementById('my-name').innerHTML = 'Mi chiamo ' + firstName + '&nbsp;' + surName;
 
 //colore preferito
 
@@ -23,10 +27,17 @@ const myColor = prompt('Inserisci colore preferito');
 
 console.log( 'La variabile myColor vale: ' + myColor);
 
+document.getElementById('my-color').innerHTML = 'Il mio colore preferito è ' + myColor;
+
 //numero fortunato
 
 const myNumber = prompt('Inserisci numero fortunato');
 
 console.log( 'La variabile myNumber vale: ' + myNumber);
 
-document.getElementById('nome').innerHTML = 'Mi chiamo ' + firstName + '&nbsp;' + surName + ' , il mio colore preferito è ' + myColor + ' e il mio numero fortunato è ' + myNumber;
+document.getElementById('my-number').innerHTML = 'Il mio numero fortunato è ' + myNumber;
+
+
+//frase unica:
+
+document.getElementById('dimostrazione-unica').innerHTML = 'Mi chiamo ' + firstName + '&nbsp;' + surName + ' , il mio colore preferito è ' + myColor + ' e il mio numero fortunato è ' + myNumber;
