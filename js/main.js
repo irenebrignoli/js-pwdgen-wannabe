c9f8495210c1ffa1f1ece60dfd44416891fd58a7
Irene Brignoli <ire.brignoli@gmail.com>
2023-02-21 15:36:54 +0100

    js finito - inizio style css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 
-/*esercizio di oggi: (insicurissimo) password generator
-nome repo: js-pwdgen-wannabe
+/*
 Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito23*/
 
+//name
+
+const firstName = prompt('Inserisci nome');
+
+console.log( 'La variabile firstName vale: ' + firstName);
+
+//surname
+
+const surName = prompt('Inserisci cognome');
+
+console.log( 'La variabile surName vale: ' + surName);
+
+//colore preferito
+
+const myColor = prompt('Inserisci colore preferito');
+
+console.log( 'La variabile myColor vale: ' + myColor);
+
+//numero fortunato
+
+const myNumber = prompt('Inserisci numero fortunato');
+
+console.log( 'La variabile myNumber vale: ' + myNumber);
+
+document.getElementById('nome').innerHTML = 'Mi chiamo ' + firstName + '&nbsp;' + surName + ' , il mio colore preferito è ' + myColor + ' e il mio numero fortunato è ' + myNumber;
